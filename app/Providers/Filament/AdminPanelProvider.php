b704c5b6e372b1851d436effeb673af897ab0ad8
Mohamed Yassin <mhmd.yassin07@gmail.com>
2025-10-25 04:14:52 +0300

    Define Resources order manually in AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -3,6 +3,17 @@
 namespace App\Providers\Filament;
 
 use App\Filament\Pages\SettingsPage;
+use App\Filament\Resources\Categories\CategoryResource;
+use App\Filament\Resources\Comments\CommentResource;
+use App\Filament\Resources\Events\EventResource;
+use App\Filament\Resources\Forms\FormResource;
+use App\Filament\Resources\FormSubmissions\FormSubmissionResource;
+use App\Filament\Resources\Pages\PageResource;
+use App\Filament\Resources\Posts\PostResource;
+use App\Filament\Resources\Services\ServiceResource;
+use App\Filament\Resources\Settings\SettingResource;
+use App\Filament\Resources\Tags\TagResource;
+use App\Filament\Resources\Users\UserResource;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -32,7 +43,19 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
-            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
+            ->resources([
+                PageResource::class,
+                PostResource::class,
+                ServiceResource::class,
+                EventResource::class,
+                CategoryResource::class,
+                TagResource::class,
+                FormResource::class,
+                FormSubmissionResource::class,
+                CommentResource::class,
+                UserResource::class,
+                SettingResource::class,
+            ])
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
                 Dashboard::class,
