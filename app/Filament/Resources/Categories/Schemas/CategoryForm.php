724c2d07c82c49b2ba6c4355f1272e558d6129c0
Mohamed Yassin <mhmd.yassin07@gmail.com>
2025-10-29 23:02:08 +0300

    Simplify category form by removing fieldsets and helper texts

--- app/Filament/Resources/Categories/Schemas/CategoryForm.php
+++ app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -5,7 +5,6 @@ namespace App\Filament\Resources\Categories\Schemas;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
-use Filament\Schemas\Components\Fieldset;
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
@@ -18,46 +17,38 @@ class CategoryForm
             ->components([
                 // Main Content Section - 8 Columns
                 Section::make('Category Information')
-                    ->description('Basic information about the category')
                     ->schema([
-                        Fieldset::make('Details')
-                            ->columns(2)
-                            ->schema([
-                                TextInput::make('name')
-                                    ->label('Name')
-                                    ->required()
-                                    ->maxLength(255)
-                                    ->live(onBlur: true)
-                                    ->columnSpan(2),
+                        TextInput::make('name')
+                            ->label('Name')
+                            ->required()
+                            ->maxLength(255)
+                            ->live(onBlur: true),
 
-                                TextInput::make('slug')
-                                    ->label('Slug')
-                                    ->required()
-                                    ->unique('categories', 'slug', ignoreRecord: true)
-                                    ->maxLength(255)
-                                    ->helperText('Auto-generated from name')
-                                    ->columnSpan(2),
+                        TextInput::make('slug')
+                            ->label('Slug')
+                            ->required()
+                            ->unique('categories', 'slug', ignoreRecord: true)
+                            ->maxLength(255),
 
-                                Textarea::make('description')
-                                    ->label('Description')
-                                    ->rows(4)
-                                    ->columnSpan(2),
-                            ]),
+                        Textarea::make('description')
+                            ->label('Description')
+                            ->rows(4),
                     ])
                     ->columnSpan(8),
 
                 // Sidebar Section - 4 Columns
                 Section::make('Category Settings')
                     ->schema([
-                        Fieldset::make('Hierarchy')
-                            ->schema([
-                                Select::make('parent_id')
-                                    ->label('Parent Category')
-                                    ->relationship('parent', 'name')
-                                    ->searchable()
-                                    ->preload()
-                                    ->helperText('Select a parent if this is a sub-category'),
-                            ]),
+                        Select::make('parent_id')
+                            ->label('Parent Category')
+                            ->options(function ($record) {
+                                return \App\Models\Category::query()
+                                    ->when($record, fn ($query) => $query->where('id', '!=', $record->id))
+                                    ->whereNull('parent_id')
+                                    ->pluck('name', 'id');
+                            })
+                            ->searchable()
+                            ->preload(),
                     ])
                     ->columnSpan(4),
             ]);
