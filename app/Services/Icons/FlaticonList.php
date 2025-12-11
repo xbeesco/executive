888d9e5a4388ab7b1b73789393a181023dc83c08
Mohamed Yassin <mhmd.yassin07@gmail.com>
2025-12-11 23:41:33 +0300

    enhance the icons lister

--- app/Services/Icons/FlaticonList.php
+++ app/Services/Icons/FlaticonList.php
@@ -5,11 +5,11 @@ namespace App\Services\Icons;
 class FlaticonList
 {
     /**
-     * Get all available Flaticon icons with their display names.
+     * Get all available Xinterio icons with their display names.
      *
-     * @return array<string, string> Array of icon class names as keys and display names as values
+     * @return array<string, string>
      */
-    public static function getIcons(): array
+    public static function getXinterioIcons(): array
     {
         return [
             'pbmit-xinterio-icon-brickwall' => 'Brickwall',
@@ -75,6 +75,228 @@ class FlaticonList
             'pbmit-xinterio-icon-office' => 'Office',
             'pbmit-xinterio-icon-wardrobe' => 'Wardrobe',
         ];
+    }
+
+    /**
+     * Get all available Base icons with their display names.
+     * These are utility icons for arrows, UI elements, social media, etc.
+     *
+     * @return array<string, string>
+     */
+    public static function getBaseIcons(): array
+    {
+        return [
+            // Arrows & Navigation
+            'pbmit-base-icon-angle-left' => 'Base: Angle Left',
+            'pbmit-base-icon-angle-right' => 'Base: Angle Right',
+            'pbmit-base-icon-angle-double-right' => 'Base: Angle Double Right',
+            'pbmit-base-icon-arrow' => 'Base: Arrow',
+            'pbmit-base-icon-arrow-left' => 'Base: Arrow Left',
+            'pbmit-base-icon-arrow-right' => 'Base: Arrow Right',
+            'pbmit-base-icon-arrow-right-1' => 'Base: Arrow Right Alt',
+            'pbmit-base-icon-black-arrow-1' => 'Base: Black Arrow',
+            'pbmit-base-icon-curve-arrow' => 'Base: Curve Arrow',
+            'pbmit-base-icon-left' => 'Base: Left',
+            'pbmit-base-icon-left-arrow' => 'Base: Left Arrow',
+            'pbmit-base-icon-left-arrow-1' => 'Base: Left Arrow Alt',
+            'pbmit-base-icon-left-arrow-2' => 'Base: Left Arrow 2',
+            'pbmit-base-icon-left-dir' => 'Base: Left Dir',
+            'pbmit-base-icon-left-open' => 'Base: Left Open',
+            'pbmit-base-icon-left-small' => 'Base: Left Small',
+            'pbmit-base-icon-next' => 'Base: Next',
+            'pbmit-base-icon-pbmit-up-arrow' => 'Base: Up Arrow',
+            'pbmit-base-icon-right' => 'Base: Right',
+            'pbmit-base-icon-right-1' => 'Base: Right Alt',
+            'pbmit-base-icon-right-arrow' => 'Base: Right Arrow',
+            'pbmit-base-icon-right-arrow-2' => 'Base: Right Arrow 2',
+            'pbmit-base-icon-right-big' => 'Base: Right Big',
+            'pbmit-base-icon-right-circled' => 'Base: Right Circled',
+            'pbmit-base-icon-right-dir' => 'Base: Right Dir',
+            'pbmit-base-icon-right-open' => 'Base: Right Open',
+            'pbmit-base-icon-right-small' => 'Base: Right Small',
+            'pbmit-base-icon-up-open-big' => 'Base: Up Open Big',
+            'pbmit-base-icon-up-right-arrow' => 'Base: Up Right Arrow',
+            'pbmit-base-icon-up-right-arrow-1' => 'Base: Up Right Arrow Alt',
+            'pbmit-base-icon-down-open-big' => 'Base: Down Open Big',
+            'pbmit-base-icon-forward' => 'Base: Forward',
+
+            // Check marks & Validation
+            'pbmit-base-icon-check' => 'Base: Check',
+            'pbmit-base-icon-check-mark' => 'Base: Check Mark',
+            'pbmit-base-icon-check-mark-2' => 'Base: Check Mark 2',
+            'pbmit-base-icon-check-solid' => 'Base: Check Solid',
+            'pbmit-base-icon-check-square-regular' => 'Base: Check Square',
+            'pbmit-base-icon-checked' => 'Base: Checked',
+            'pbmit-base-icon-ok-squared' => 'Base: OK Squared',
+            'pbmit-base-icon-tick' => 'Base: Tick',
+            'pbmit-base-icon-tick-1' => 'Base: Tick Alt',
+
+            // Close & Cancel
+            'pbmit-base-icon-cancel' => 'Base: Cancel',
+            'pbmit-base-icon-close-circular-button-symbol' => 'Base: Close Circular',
+            'pbmit-base-icon-cross' => 'Base: Cross',
+
+            // Plus & Add
+            'pbmit-base-icon-plus' => 'Base: Plus',
+            'pbmit-base-icon-plus-button' => 'Base: Plus Button',
+            'pbmit-base-icon-plus-symbol-button' => 'Base: Plus Symbol',
+
+            // Phone & Communication
+            'pbmit-base-icon-phone' => 'Base: Phone',
+            'pbmit-base-icon-phone-call' => 'Base: Phone Call',
+            'pbmit-base-icon-phone-call-1' => 'Base: Phone Call Alt',
+            'pbmit-base-icon-phone-volume-solid' => 'Base: Phone Volume',
+            'pbmit-base-icon-phone-volume-solid-1' => 'Base: Phone Volume Alt',
+            'pbmit-base-icon-phone-auricular-and-clock-delivery-symbol' => 'Base: Phone 24/7',
+
+            // Email & Messages
+            'pbmit-base-icon-email' => 'Base: Email',
+            'pbmit-base-icon-letter' => 'Base: Letter',
+            'pbmit-base-icon-mail-alt' => 'Base: Mail Alt',
+            'pbmit-base-icon-arroba' => 'Base: At Sign',
+            'pbmit-base-icon-paper-plane' => 'Base: Paper Plane',
+
+            // Chat & Comments
+            'pbmit-base-icon-chat' => 'Base: Chat',
+            'pbmit-base-icon-chat-1' => 'Base: Chat Alt',
+            'pbmit-base-icon-chat-2' => 'Base: Chat 2',
+            'pbmit-base-icon-chat-3' => 'Base: Chat 3',
+            'pbmit-base-icon-chat-bubble' => 'Base: Chat Bubble',
+            'pbmit-base-icon-chat-bubble-1' => 'Base: Chat Bubble Alt',
+            'pbmit-base-icon-chat-empty' => 'Base: Chat Empty',
+            'pbmit-base-icon-comment' => 'Base: Comment',
+            'pbmit-base-icon-comment-1' => 'Base: Comment Alt',
+            'pbmit-base-icon-comment-alt' => 'Base: Comment Square',
+            'pbmit-base-icon-comment-empty' => 'Base: Comment Empty',
+            'pbmit-base-icon-speech-bubble' => 'Base: Speech Bubble',
+            'pbmit-base-icon-testimonial' => 'Base: Testimonial',
+
+            // Location & Map
+            'pbmit-base-icon-location' => 'Base: Location',
+            'pbmit-base-icon-location-1' => 'Base: Location Alt',
+            'pbmit-base-icon-location-2' => 'Base: Location 2',
+            'pbmit-base-icon-location-dot-solid' => 'Base: Location Dot',
+            'pbmit-base-icon-marker' => 'Base: Marker',
+            'pbmit-base-icon-pin' => 'Base: Pin',
+            'pbmit-base-icon-pin-outline' => 'Base: Pin Outline',
+            'pbmit-base-icon-placeholder' => 'Base: Placeholder',
+            'pbmit-base-icon-placeholder-1' => 'Base: Placeholder Alt',
+
+            // Calendar & Time
+            'pbmit-base-icon-calendar' => 'Base: Calendar',
+            'pbmit-base-icon-calendar-1' => 'Base: Calendar Alt',
+            'pbmit-base-icon-calendar-2' => 'Base: Calendar 2',
+            'pbmit-base-icon-calendar-3' => 'Base: Calendar 3',
+            'pbmit-base-icon-calendar-alt-solid' => 'Base: Calendar Solid',
+            'pbmit-base-icon-appointment' => 'Base: Appointment',
+            'pbmit-base-icon-clock' => 'Base: Clock',
+            'pbmit-base-icon-clock-1' => 'Base: Clock Alt',
+            'pbmit-base-icon-clock-2' => 'Base: Clock 2',
+            'pbmit-base-icon-clock-3' => 'Base: Clock 3',
+            'pbmit-base-icon-time' => 'Base: Time',
+
+            // User & Profile
+            'pbmit-base-icon-user' => 'Base: User',
+            'pbmit-base-icon-user-1' => 'Base: User Alt',
+            'pbmit-base-icon-user-2' => 'Base: User 2',
+            'pbmit-base-icon-user-3' => 'Base: User 3',
+            'pbmit-base-icon-user-4' => 'Base: User 4',
+            'pbmit-base-icon-user-circle' => 'Base: User Circle',
+            'pbmit-base-icon-contact' => 'Base: Contact',
+
+            // Shopping & Cart
+            'pbmit-base-icon-cart-bag' => 'Base: Cart Bag',
+            'pbmit-base-icon-shopping-bag' => 'Base: Shopping Bag',
+            'pbmit-base-icon-shopping-cart' => 'Base: Shopping Cart',
+            'pbmit-base-icon-supermarket' => 'Base: Supermarket',
+            'pbmit-base-icon-supermarket-1' => 'Base: Supermarket Alt',
+            'pbmit-base-icon-supermarket-2' => 'Base: Supermarket 2',
+
+            // Media & Play
+            'pbmit-base-icon-play-button' => 'Base: Play Button',
+            'pbmit-base-icon-play-button-1' => 'Base: Play Button Alt',
+            'pbmit-base-icon-videocam' => 'Base: Video Camera',
+            'pbmit-base-icon-music' => 'Base: Music',
+            'pbmit-base-icon-gallery' => 'Base: Gallery',
+
+            // Documents & Files
+            'pbmit-base-icon-doc' => 'Base: Doc',
+            'pbmit-base-icon-doc-file-format' => 'Base: Doc File',
+            'pbmit-base-icon-document' => 'Base: Document',
+            'pbmit-base-icon-file' => 'Base: File',
+            'pbmit-base-icon-folder-open' => 'Base: Folder Open',
+            'pbmit-base-icon-folder-open-empty' => 'Base: Folder Empty',
+            'pbmit-base-icon-pdf-file-format-symbol' => 'Base: PDF',
+            'pbmit-base-icon-pdf-file-format-symbol-1' => 'Base: PDF Alt',
+            'pbmit-base-icon-news' => 'Base: News',
+            'pbmit-base-icon-form' => 'Base: Form',
+
+            // Social Media
+            'pbmit-base-icon-facebook-f' => 'Base: Facebook',
+            'pbmit-base-icon-facebook-squared' => 'Base: Facebook Squared',
+            'pbmit-base-icon-instagram' => 'Base: Instagram',
+            'pbmit-base-icon-twitter' => 'Base: Twitter',
+            'pbmit-base-icon-twitter-2' => 'Base: Twitter Alt',
+            'pbmit-base-icon-linkedin-in' => 'Base: LinkedIn',
+            'pbmit-base-icon-linkedin-squared' => 'Base: LinkedIn Squared',
+            'pbmit-base-icon-youtube-play' => 'Base: YouTube',
+            'pbmit-base-icon-pinterest' => 'Base: Pinterest',
+            'pbmit-base-icon-reddit' => 'Base: Reddit',
+            'pbmit-base-icon-tumbler' => 'Base: Tumblr',
+            'pbmit-base-icon-vimeo-1' => 'Base: Vimeo',
+            'pbmit-base-icon-flickr' => 'Base: Flickr',
+            'pbmit-base-icon-gplus' => 'Base: Google Plus',
+
+            // Quote & Text
+            'pbmit-base-icon-left-quote' => 'Base: Left Quote',
+            'pbmit-base-icon-quote' => 'Base: Quote',
+            'pbmit-base-icon-quote-2' => 'Base: Quote Alt',
+            'pbmit-base-icon-quote-left' => 'Base: Quote Left',
+            'pbmit-base-icon-right-quotation-mark' => 'Base: Quote Right',
+            'pbmit-base-icon-align-left' => 'Base: Align Left',
+
+            // Search & Actions
+            'pbmit-base-icon-search' => 'Base: Search',
+            'pbmit-base-icon-search-1' => 'Base: Search Alt',
+            'pbmit-base-icon-download' => 'Base: Download',
+            'pbmit-base-icon-download-solid' => 'Base: Download Solid',
+            'pbmit-base-icon-link' => 'Base: Link',
+            'pbmit-base-icon-link-1' => 'Base: Link Alt',
+            'pbmit-base-icon-share' => 'Base: Share',
+            'pbmit-base-icon-share-1' => 'Base: Share Alt',
+            'pbmit-base-icon-share-2' => 'Base: Share 2',
+            'pbmit-base-icon-reply' => 'Base: Reply',
+
+            // Heart & Favorites
+            'pbmit-base-icon-heart' => 'Base: Heart',
+            'pbmit-base-icon-heart-empty' => 'Base: Heart Empty',
+            'pbmit-base-icon-bookmark' => 'Base: Bookmark',
+            'pbmit-base-icon-star' => 'Base: Star',
+            'pbmit-base-icon-star-1' => 'Base: Star Alt',
+            'pbmit-base-icon-pointed-star' => 'Base: Pointed Star',
+            'pbmit-base-icon-xinterio-star' => 'Base: Xinterio Star',
+
+            // Misc UI Elements
+            'pbmit-base-icon-aside' => 'Base: Aside',
+            'pbmit-base-icon-d1' => 'Base: D1',
+            'pbmit-base-icon-full-stop' => 'Base: Full Stop',
+            'pbmit-base-icon-menu' => 'Base: Menu',
+            'pbmit-base-icon-menu-1' => 'Base: Menu Alt',
+            'pbmit-base-icon-method-draw-image' => 'Base: Draw Image',
+            'pbmit-base-icon-square-dot-' => 'Base: Square Dot',
+            'pbmit-base-icon-sticky' => 'Base: Sticky',
+            'pbmit-base-icon-tags' => 'Base: Tags',
+        ];
+    }
+
+    /**
+     * Get all available Flaticon icons with their display names (combined).
+     *
+     * @return array<string, string> Array of icon class names as keys and display names as values
+     */
+    public static function getIcons(): array
+    {
+        return array_merge(self::getXinterioIcons(), self::getBaseIcons());
     }
 
     /**
